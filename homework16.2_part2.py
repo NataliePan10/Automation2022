43d455cf05763395dd90a496c0cfd489a8a3e59e
NataliePan10 <106000965+NataliePan10@users.noreply.github.com>
2022-07-15 23:15:28 -0400

    git commit

---
+++ homework16.2_part2.py
@@ -0,0 +1,17 @@
+# Define shout_echo
+def shout_echo(word1, value1):
+    print(word1, value1)
+shout_echo("echo", "!!!")
+
+
+# Concatenate '!!!' to echo_word: shout_word
+def shout_echo(str, shout_word):
+    print(str, shout_word)
+
+shout_echo("hey", shout_word = "echo_word" + '!!!')
+
+
+# Return shout_word
+def shout_echo(value1)
+    return shout_echo
+
--- homework16.3.py
+++ homework16.3.py
@@ -1,1 +1,30 @@
 #exercise 1
+clothes = ["socks", "shirt", "skirt", "scarf"]
+clothes.append("new_cloth",)
+print(clothes)
+
+clothes = ["socks", "shirt", "skirt", "scarf"]
+clothes.insert( 0, "new_cloth")
+print(clothes)
+
+clothes = ["new_cloth", "socks", "shirt", "skirt", "scarf"]
+print(clothes.pop(1))
+print(clothes.pop(3))
+print(clothes.pop(-1))
+print(clothes.pop(0))
+
+
+#Exercise 2
+
+employee_shift = ["Mike", "Andrew", "Emma", "Kelly", "John", "Brad"]
+
+old_employee = ["Brad"]
+new_employee = ["Natalie"]
+employee_shift [5] = "Natalie"
+new_employee = (employee_shift)
+print(new_employee)
+print(employee_shift)
+employee_shift = ["Mike", "Andrew", "Emma", "Kelly", "John", "Brad"]
+employee_shift [3] =  "Maria"
+print(employee_shift)
+
