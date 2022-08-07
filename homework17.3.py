33bfb95e014d0a9bcb9fd87cd8286e25d732a22a
NataliePan10 <106000965+NataliePan10@users.noreply.github.com>
2022-08-07 01:26:57 -0400

    git commit

--- homework17.3.py
+++ homework17.3.py
@@ -1,7 +1,29 @@
 
-user_input = list1()
-    def x (list1):
-        for x in list1:
-            print(X)
-list1=(10, 20, 30)
+list1 = []
+for e in range(1,4):
+    list1.append(e)
+    print(e)
+
+
+total = 0
+for e in list1:
+    total = total + e
+print(total)
+
+
+def print_sum_avg(num):
+    sum_num = 0
+    for t in num:
+        sum_num = sum_num + t
+
+    avg = sum_num / len(num)
+    return avg
+
+print("The average is", print_sum_avg([1,2,3]))
+
+def print_sum_avg_ars(*args):
+    return sum(args, 0.0) / len(args)
+print (print_sum_avg_ars(1, 2, 3))
+
+
 
