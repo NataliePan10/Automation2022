c870a7527a7462da6b3c93b6b631b50a266945a3
NataliePan10 <106000965+NataliePan10@users.noreply.github.com>
2022-08-09 14:26:02 -0400

    git commit

--- ch17.3ex4.py
+++ ch17.3ex4.py
@@ -3,7 +3,7 @@ number_to_win = 27
 user_input = input("Guess the winning number from 20 to 30\n")
 while int(user_input) != number_to_win:
     print( "You are wrong! Try one more time!")
-    user_input + input("Guess the winning number from 20 to 30\n")
+    user_input = input("Guess the winning number from 20 to 30\n")
     print(" Not your luck! Try again!")
     user_input = input("Last chance!!!Guess the winning number from 20 to 30\n")
 
---
+++ homework18.1.py
@@ -0,0 +1,3 @@
+
+number_to_guess = 37
+user_input = input("Guess the winning number from 20 to 30\n")
