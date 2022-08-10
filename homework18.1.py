12ae6c4c3328b84557814a61a9c4f51a85e13c68
NataliePan10 <106000965+NataliePan10@users.noreply.github.com>
2022-08-09 23:13:55 -0400

    git commit

--- homework18.1.py
+++ homework18.1.py
@@ -1,3 +1,13 @@
+import random
 
-number_to_guess = 37
-user_input = input("Guess the winning number from 20 to 30\n")
+random_number = random.randint(0, 50)
+for attempt in range(7):
+    user_input = int(input("Guess the number from 0 to 50:"))
+    if user_input < random_number:
+        print("The value is too low")
+    elif user_input > random_number:
+        print("The value is too high")
+    elif user_input == random_number:
+        print("You guessed correctly")
+        break
+
